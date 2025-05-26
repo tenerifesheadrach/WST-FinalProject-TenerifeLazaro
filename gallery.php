720d12a79d45c1442e25c22a78abcb71b5f9570a
yesh <yeshanicolelazarooo@gmail.com>
2025-05-26 18:18:19 +0800

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -23,7 +23,7 @@ $xml = simplexml_load_file('employee.xml');
     /* Navbar styles */
     .navbar {
       width: 100%;
-      background-color: #0d47a1;
+      background-color: #ff7029;
       color: #fff;
       padding: 1rem 2rem;
       display: flex;
